e1f668e5c676ebbdc2979e5cf637ccf210b02cf8
zhaoyuhua <2072224807@qq.com>
2014-09-22 18:04:10 +0800

    客户列表 分页   样式修改

--- Application/Home/Controller/CustomerController.class.php
+++ Application/Home/Controller/CustomerController.class.php
@@ -1,10 +1,104 @@
 <?php
 namespace Home\Controller;
+use Think\Page;
 use Common\Controller\HomeBaseController;
+use Home\Model\CustomerModel;
 
 class CustomerController extends HomeBaseController {
     /**客户添加***/
 	public function add(){
+		$this->display();					
+	}
+	
+	/***新增的客户接口***/
+	public function insert(){		
+		//$data['user_id']  = UID;		
+		$data['add_time'] = time();
+		$model = new CustomerModel();	
+		$data = $model->create();
+		$model->add($data);
+		$this->success('添加成功',U('Customer/lists'));
+	}
+	
+	
+	/**客户列表     公司名称搜索**/
+	public function lists($name=null, $status=1, $tpl=null) {				
+		$map=array();
+		$cust = M('Customer');
+		$map['status'] = $status;
+		$id = (int)I('param.id');
+		if ($id>0) $map['id'] = $id;		
+		if(isset($name)){
+			$map['name']   =   array('like', '%'.$name.'%');
+		}	
+		
+		$count      = $cust->where($map)->count();// 查询满足要求的总记录数
+		$Page       = new \Think\Page($count,2);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+		$show       = $Page->show();// 分页显示输出// 进行分页数据查询 注意limit方法的参数要使用Page类的属性
+		$list = $cust->where($map)->limit($Page->firstRow.','.$Page->listRows)->select();
+		$this->assign('list',$list);// 赋值数据集
+		$this->assign('page',$show);// 赋值分页输出
+		
+// 		$list = $cust->where($map)->select();						
+// 		$this->assign('list',$list);
+		$this->assign('status', $map['status']); //用于搜索条件的显示
+		$this->assign('name', $name);           //用于搜索条件的显示
+		if (isset($tpl)) $this->display($tpl);
+        else $this->display();
+	}
+	
+	/***客户名称模糊检索***/
+	public function search($name=''){
+		$name = I('name');
+// 		print_r($name);
+// 		exit();
+// 		if (!empty($name)) {
+// 			$Form = M("Customer");
+// 			if ($Form->getByName($name)) {
+// 				$this->error('名称已经存在');
+// 			} else {
+// 				$this->success('名称可以使用!');
+// 			}
+// 			} else {
+// 				$this->error('名称必须');
+// 			}
+				$this->display();		
+		
+	}
+	/**公司信息修改**/
+	public function edit(){
+		$cust = M('Customer');
+		$id = (int)I('id');
+		$list = $cust->where($id)->find();
+		$this->assign('list',$list);
+        $this->display();
+	}
+	/**公司信息修改提交  接口**/
+	public function update(){
+		$id = (int)I('param.id');
+		if ($id <= 0) $this->error('参数非法');		
+		$newdata = array();
+		$newdata['id'] = I('param.id');
+		$newdata['name'] = I('param.name');
+		$newdata['contacts'] = I('param.contacts');
+		$newdata['phone'] = I('param.phone');
+		$newdata['fax'] = I('param.fax');
+		$newdata['address'] = I('param.address');	
+		$model = M('Customer');
+		if (false === $model->create($newdata)) $this->error($model->getError());
+		if (false === $model->where('id='.$id)->save()) $this->error('更新失败');
+		$this->success('更新成功');
+	}
+	/*查看详情*/
+	public function detailed(){
+		$cust = M('Customer');
+		$id = (int)I('id');
+// 		print_r($id);
+// 		exit();
+		$list = $cust->where($id)->find();
+// 						print_r($list);
+// 						exit();
+		$this->assign('list',$list);
 		$this->display();
 	}
 	
