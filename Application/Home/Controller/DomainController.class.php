15b1c304154d2a950c988992be48751956043dff
zhaoyuhua <2072224807@qq.com>
2014-09-26 18:00:02 +0800

    域名添加

--- Application/Home/Controller/DomainController.class.php
+++ Application/Home/Controller/DomainController.class.php
@@ -6,4 +6,27 @@ class DomainController extends HomeBaseController{
 	public function domain_list(){
 		$this->display();
 	}
+	/*添加域名*/
+	public function add_domain(){
+		$this->display();
+	}
+	
+	/**提交 待审申请**/
+	public function check(){
+		$id = (int)I('param.id');
+		if ($id <= 0) $this->error('参数非法');
+		$newdata = array();
+		$newdata['id'] = I('param.id');
+		$newdata['check'] = 1;
+		$newdata['check_time'] = time();
+		$model = M('Domain');
+		if (false === $model->create($newdata)) $this->error($model->getError());
+		if (false === $model->where('id='.$id)->save()) $this->error('审核失败');
+		$this->success('审核成功',U('Domain/domain_list'));
+		// 		print_r($newdata['check_time']);
+		// 		exit();
+	}
+	
+	
+	
 }     
