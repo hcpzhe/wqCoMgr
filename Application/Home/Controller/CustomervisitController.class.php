d25dd832f5cba0058ab76c1ac587e34f9667bfeb
zhaoyuhua <2072224807@qq.com>
2014-09-28 10:24:37 +0800

    拜访记录 列表页和详情页

--- Application/Home/Controller/CustomervisitController.class.php
+++ Application/Home/Controller/CustomervisitController.class.php
@@ -3,22 +3,24 @@ namespace Home\Controller;
 use Think\Page;
 use Common\Controller\HomeBaseController;
 use Home\Model\Customer_visitModel;
+use Home\Model\Visit_prodModel;
 
 class CustomervisitController extends HomeBaseController {
 
 	/**拜访记录列表**/
 	public function visitlists(){
-// 		$visit = M('customer_visit');		
-// 		$count      = $visit->count();// 查询满足要求的总记录数
-// 		$Page       = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数(10)
-// 		$show       = $Page->show();// 分页显示输出
-// 		$visit_list = $visit->limit($Page->firstRow.','.$Page->listRows)->order('visit_time desc')->select();				
-// 		$this->assign('visit_lisit',$visit_list);
-// 		$this->display();
-
-		$visit=new Customer_visitModel();
-		$this->data=$visit->visitlist();
-		
+		$visit=new Customer_visitModel();				
+        $count=$visit->table('erp_customer_visit as cu,erp_user as ur,erp_customer as cr')
+		->where("cu.user_id=ur.id AND cu.cust_id=cr.id")		
+		->count();       // 查询满足要求的总记录数        
+		$Page       = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数(10)
+		$show       = $Page->show();// 分页显示输出
+		$visit_list = $visit->table('erp_customer_visit as cu,erp_user as ur,erp_customer as cr')
+		->where("cu.user_id=ur.id AND cu.cust_id=cr.id")
+		->field("cu.id as id,ur.realname as uname,cr.`name` as cname,cu.visit_time,cu.`content`")
+		->limit($Page->firstRow.','.$Page->listRows)->order('visit_time desc')->select();       
+ 		$this->assign('visit_lisit',$visit_list);
+        $this->assign('show',$show);   //分页显示
 		$this->display();
 		
 	}
@@ -30,8 +32,6 @@ class CustomervisitController extends HomeBaseController {
 		$cust_name = $cust->where('id='.$cust_id)->getField('name');
 		$this->assign('cust_id',$cust_id);
 		$this->assign('cust_name',$cust_name);
-// 		print_r($cust_name);
-// 		exit();
 		$this->display();
 		
 	}
@@ -53,32 +53,52 @@ class CustomervisitController extends HomeBaseController {
 	
 	/**查看拜访记录的详细信息**/
 	public function visit_detailed(){
-		$id = (int)I('id');
+		$visit_id = (int)I('id');
 		$model = new Customer_visitModel();  //拜访的详细信息
 		$data = $model->table('erp_customer_visit as cu,erp_user as ur,erp_customer as cr')
-		->where("cu.user_id=ur.id AND cu.cust_id=cr.id AND cu.id=$id")
-		->getField("cu.id as id,ur.realname as uname,cr.`name` as cname,cu.visit_time,cu.`content`");		
+		->where("cu.user_id=ur.id AND cu.cust_id=cr.id AND cu.id=$visit_id")
+		->getField("cu.id as id,ur.realname as uname,cr.name as cname,cu.visit_time,cu.content");	
+// 		print_r($data);
+// 		exit();	
 		$this->assign('data',$data);
 		
-		
-		//$visit_prod = M('visit_prod');   //沟通记录
-		//$visit_prod_list = $visit_prod->where('visit_id='.$id)->select();
+		//与拜访记录有关的沟通信息
 		$visit_prod_list = $model->table('erp_customer_visit as cu,erp_product as pt,erp_visit_prod as vd')
-		->where("cu.id=vd.visit_id AND pt.id=vd.prod_id AND cu.id=$id")
-		->getField("pt.name as pname,vd.content as content");
+		->where("cu.id=vd.visit_id AND pt.id=vd.prod_id AND cu.id=$visit_id")
+		->select();
 // 		print_r($visit_prod_list);
 // 		exit();
-		
+
 		$this->assign('visit_prod_list',$visit_prod_list);
 		$this->display();
 
 	}
-	
+	/**添加沟通记录**/
 	public function add_visit_prod(){
+		$product = M('product');
+		$product_list = $product->select();
+		$this->assign('product_list',$product_list);
+// 		print_r($product_name);
+// 		exit();
+		
 		$visit_id = (int)I('visit_id');
-		print_r($visit_id);
-		exit();
+		$newdata = array();
+		$newdata['visit_id'] = I('param.visit_id');
+		$newdata['uname'] = I('param.uname');
+		$newdata['cname'] = I('param.cname');
+		$newdata['visit_time'] = I('param.visit_time');
+		$this->assign('newdata',$newdata);
+		
+		
+		$this->display();
 	}
-	
+	/**沟通记录添加   提交接口**/
+	public function visit_prod_insert(){
+		$model = new Visit_prodModel(); 
+		$data = $model->create();
+		$model->add($data);
+		$this->success('添加成功',U('Customervisit/visitlists'));
+		
+	}
 	
 }
--- Application/Home/Model/Customer_visitModel.class.php
+++ Application/Home/Model/Customer_visitModel.class.php
@@ -17,23 +17,5 @@ class Customer_visitModel extends Model {
 	
 	protected $_auto		=	array(
 			array('visit_time','time',self::MODEL_INSERT,'function'),
-	);
-	 
-    /**拜访记录列表**/
-	Public function visitlist(){
-		/*查询数据总条数*/
-		$count=$this->table('erp_customer_visit as cu,erp_user as ur,erp_customer as cr')
-		->where("cu.user_id=ur.id AND cu.cust_id=cr.id")
-		->count();
-		/*载入分页类，初始化数据*/
-		$page=new \Think\Page($count,10);
-		/*调用分页链接函数*/
-		$data['show']=$page->show();
-		/*控制数据查询条数*/
-		$data['visit_list']=$this->table('erp_customer_visit as cu,erp_user as ur,erp_customer as cr')
-		->where("cu.user_id=ur.id AND cu.cust_id=cr.id")
-		->field("cu.id as id,ur.realname as uname,cr.`name` as cname,cu.visit_time,cu.`content`")
-		->limit($page->firstRow.','.$page->listRows)->order('visit_time desc')->select();
-		return $data;
-	}
+	);	 
 }
