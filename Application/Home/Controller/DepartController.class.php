efb1b45bbe4954fd09463c3c7c29add523869c04
wcd <774241885@qq.com>
2014-10-08 15:12:17 +0800

    修改  部门修改 提交  部门删除

--- Application/Home/Controller/DepartController.class.php
+++ Application/Home/Controller/DepartController.class.php
@@ -22,8 +22,8 @@ class DepartController extends HomeBaseController{
 		$map['parent_id']=$_POST['parent_id'];
 		$depart=new DepartModel();
 		$flag=$depart->add($map);
-		if($flag==0){	$this->error('添加失败！');
-		}else{	$this->success('添加成功！');}
+		if($flag==0){	$this->success('添加失败！');
+		}else{	$this->error('添加成功！');}
 	}
 	/** 跳转到编辑页面 */
 	public function up_dp_form(){
@@ -38,17 +38,22 @@ class DepartController extends HomeBaseController{
 		$this->display();
 	}
 	/** 编辑部门操作 */
-	public function  up_dp(){
+	public function up_dp(){
 		$id=$_POST['id'];
 		$map['name']=$_POST['name'];
 		$map['parent_id']=$_POST['parent_id'];
 		$depart=new DepartModel();
 		$flag=$depart->where("id=$id")->save($map);
-		if($flag==0){	$this->error('修改失败！');
-		}else{	$this->success('修改成功！');}
+		if($flag==1){	$this->success('修改成功！');
+		}else{	$this->error('修改失败！');}
 	}
 	/** 部门删除 */
 	public function del_dp(){
-		
+		$id=$_GET['id'];
+		$depart=new DepartModel();
+		$flag=$depart->where("id=$id")->setField('status','-1');
+// 		echo $depart->_sql();exit();
+		if($flag==1){	$this->success('删除成功！');
+		}else{	$this->error('删除失败！');}
 	}
 }
