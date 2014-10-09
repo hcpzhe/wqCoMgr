8314ba78001d06b1df5132568cf4d90b0589232c
wcd <774241885@qq.com>
2014-10-09 11:48:24 +0800

    产品删除    提交

--- Application/Home/Controller/ProductController.class.php
+++ Application/Home/Controller/ProductController.class.php
@@ -9,4 +9,11 @@ class ProductController extends HomeBaseController{
 		$this->pro_list=$product->p_list();
 		$this->display();
 	}
+	/** 删除产品 */
+	public function del_pro($id){
+		$product=new ProductModel();
+		$flag=$product->where("id=$id")->setField('status','-1');
+		if($flag==1){	$this->success('删除成功！');
+		}else{	$this->error('删除失败！');}
+	}
 }
