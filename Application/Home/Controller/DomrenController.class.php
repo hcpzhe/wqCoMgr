1d9ecde20671d9a67ecd0c3a8f3e2cbc1b1f0e53
zhaoyuhua <2072224807@qq.com>
2014-10-10 17:06:04 +0800

    域名申请续费  分页

--- Application/Home/Controller/DomrenController.class.php
+++ Application/Home/Controller/DomrenController.class.php
@@ -7,22 +7,34 @@ use Think\Page;
 class DomrenController extends HomeBaseController{
 	/*域名续费申请  列表*/
 	public function dom_ren_app(){
-		$model = M('Domain_renewal');
-				
+		$model = M('Domain_renewal');		
+		$count=$model->table('erp_domain as dn,erp_domain_renewal as dr')
+		->where("dn.id=dr.domain_id AND 'status'=0 or 'status'=1")
+		->count();       // 查询满足要求的总记录数
+		$Page       = new \Think\Page($count,10);// 实例化分页类 传入总记录数和每页显示的记录数(10)
+		$show       = $Page->show();// 分页显示输出
+		//续费申请的信息		
 		$apply_list = $model->table('erp_domain as dn,erp_domain_renewal as dr')
 		->where("dn.id=dr.domain_id AND 'status'=0 or 'status'=1")
+		->order('dr.id desc')
 		->getField("dr.id,dr.domain_id,dn.domain,dr.money,dr.org_expired_time,dr.new_expired_time,dr.pay_time,dr.`check`");
-		
         $this->assign('apply_list',$apply_list);
+        $this->assign('show',$show);   //分页显示
 		$this->display();
 	}
 	/**域名申请续费 **/
 	public function apply_renewal(){
-		$domain_id = (int)I('id');
+		$domain_id = (int)I('id');		
 		$domain = M('Domain');
+		$check = $domain->where('id='.$domain_id)->getField('check');
+		//域名审核通过后 才能提交续费申请
+		if ($check == 0 || $check == -1){
+			$this->redirect('Domain/domain_list',array('id'=>$domain_id),1,'域名还未经过审核,请审核！'); 
+		}else {
 		$domain_mess = $domain->where('id='.$domain_id)->find();
 		$this->assign('domain_mess',$domain_mess);
 		$this->display();
+		}
 	}
 	/**域名续费申请   提交接口**/
 	public function insert(){
@@ -31,11 +43,13 @@ class DomrenController extends HomeBaseController{
 		$data['org_expired_time'] =  I('param.expired_time');  //原过期时间
 		$data['year_num'] = I('param.year');   //域名续费年限
 		$data['new_expired_time'] = $data['org_expired_time'] + $data['year_num']*60*60*24*365;  //域名到期时间计算      新过期时间
-		$data['pay_time'] = I('param.pay_time');;
+		$data['pay_time'] = I('param.pay_time');
+		if ($data['org_expired_time'] < time())
+		$data['status'] = 0;
 		$model = M('Domain_renewal');
 		$data = $model->data($data)->add();
 	
-		$this->success('域名续费申请成功',U('Domain/domain_list'));
+		$this->success('域名续费申请成功',U('Domren/dom_ren_app'));
 	}
 	
 	/**提交 待审申请**/
@@ -53,8 +67,7 @@ class DomrenController extends HomeBaseController{
 		$domain_expired_time = $apply_list[$id]['new_expired_time'];   //域名新过期时间
 		$domain_M = M('Domain');
 		$domain_M -> where('id='.$domain_id)->setField('expired_time',$domain_expired_time);
-		
-		
+				
 		//提交续费申请
 		$newdata = array();
 		$newdata['id'] = I('param.id');
@@ -69,29 +82,15 @@ class DomrenController extends HomeBaseController{
 	
 	/**查看域名申请的详细信息***/
 	public function domren_apply_detailed(){
-		$id = (int)I('param.id');
-		
+		$id = (int)I('param.id');		
 		$model = M('Domain_renewal');
 		
 		$apply_list = $model->table('erp_domain as dn,erp_domain_renewal as dr,erp_customer as cu')
 		->where("dn.id=dr.domain_id AND cu.id=dn.cust_id AND dr.id=$id")
 		->getField("dr.id,dr.domain_id,dn.domain,dn.service,dn.reg_time,dr.money,dr.org_expired_time,dr.new_expired_time,dr.pay_time,dr.`check`,dr.check_time,cu.name,cu.contacts,cu.phone");
-		
-// 		print_r($apply_list);
-// 		exit();
-	   $this->assign('apply_list',$apply_list);
+	    $this->assign('apply_list',$apply_list);
 		$this->display();	
 		
-	}
-	
-	/***域名续费申请编辑***/
-	public function domain_apply_edit(){
-
-		$id = (int)I('param.id');
-		print_r($id);
-		exit();
-		
-		$this->display();
 	}
 	
 }
