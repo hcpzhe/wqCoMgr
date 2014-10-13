57305eb6b747d1c702fcc3049c5812c92a2cc05c
wcd <774241885@qq.com>
2014-10-13 16:20:56 +0800

    付款记录列表操作方式更改，添加搜索

--- Application/Home/Controller/OrderpayController.class.php
+++ Application/Home/Controller/OrderpayController.class.php
@@ -5,8 +5,26 @@ use Home\Model\Order_payModel;
 class OrderpayController extends HomeBaseController{
 	/*订单付款记录*/
 	public function order_pay_record(){
+		/*获取搜索条件*/
+		$class=$_POST['class'];
+		$check=$_POST['check'];
+		$key=$_POST['key'];
+		/** 拼接where条件 */
+		if(!empty($class)){
+			$where=$where." AND op.class=".$class;
+		}
+		/*通过，待审核*/
+		if($check==1){
+			$where=$where." AND op.check=1";
+		}else if($check==2){
+			$where=$where." AND op.check=0";
+		}else{ $where=$where." AND op.check>=0";}
+		
+		if(!empty($key)){
+			$where=$where." AND ( cr.name like '%".$key."%' or ur.realname like '%".$key."%')";
+		}
 		$orderpay=new Order_payModel();
-		$this->data=$orderpay->op_record();
+		$this->data=$orderpay->op_record($where);
 		$this->display();
 	}
 	/*订单付款详情*/
