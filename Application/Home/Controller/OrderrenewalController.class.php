ac215e9b80985d6d617e79f7766b80ad14b50926
wcd <774241885@qq.com>
2014-10-14 10:31:54 +0800

    续费申请列表 操作方式更改  添加搜索

--- Application/Home/Controller/OrderrenewalController.class.php
+++ Application/Home/Controller/OrderrenewalController.class.php
@@ -3,11 +3,33 @@ namespace Home\Controller;
 use Common\Controller\HomeBaseController;
 use Home\Model\Order_renewalModel;
 use Home\Model\OrderModel;
+use Home\Model\ProductModel;
 class OrderrenewalController extends HomeBaseController{
 	/** 订单续费申请列表 */
 	public function or_list(){
+		/*获取搜索条件*/
+		$pro=$_POST['product'];
+		$check=$_POST['check'];
+		$key=$_POST['key'];
+		/** 拼接where条件 */
+		if(!empty($pro)){
+			$where=$where." AND pt.id=".$pro;
+		}
+		/*通过，待审核*/
+		if($check==1){
+			$where=$where." AND eor.check=1";
+		}else if($check==2){
+			$where=$where." AND eor.check=0";
+		}else{ $where=$where." AND eor.check>=0";}
+		
+		if(!empty($key)){
+ 			$where=$where." AND ( pt.name like '%".$key."%' or cr.name like '%".$key."%')";
+		}
 		$Ordren=new Order_renewalModel();
-		$this->or=$Ordren->orlist();
+		$this->or=$Ordren->orlist($where);
+		/*产品列表*/
+		$product=new ProductModel();
+		$this->pro_list=$product->p_list();
 		$this->display();
 	}
 	/** 续费申请表单rene */
--- Application/Home/Model/Order_renewalModel.class.php
+++ Application/Home/Model/Order_renewalModel.class.php
@@ -3,10 +3,11 @@ namespace Home\Model;
 use Think\Model;
 class Order_renewalModel extends  Model{
 	/** 订单续费申请列表 */
-	public function orlist(){
+	public function orlist($where){
+		$where="oe.cust_id=cr.id AND oe.prod_id=pt.id AND eor.order_id=oe.id ".$where;
 		/*查询数据总条数*/
 		$count=$this->table('erp_order as oe,erp_customer as cr,erp_product as pt,erp_order_renewal as eor')
-		->where("oe.cust_id=cr.id AND oe.prod_id=pt.id AND eor.order_id=oe.id")
+		->where($where)
 		->count();
 // 		echo $this->_sql();exit();
 		/*载入分页类，初始化数据*/
@@ -15,10 +16,10 @@ class Order_renewalModel extends  Model{
 		$data['show']=$page->show();
 		/*控制数据查询条数*/
 		$data['order_list']=$this->table('erp_order as oe,erp_customer as cr,erp_product as pt,erp_order_renewal as eor')
-		->where("oe.cust_id=cr.id AND oe.prod_id=pt.id AND eor.order_id=oe.id")
+		->where($where)
 		->field("oe.id as id,cr.`name` as cname,pt.`name` as pname,pt.`id` as pid,eor.money as money,eor.check")
 		->limit($page->firstRow.','.$page->listRows)->select();
-// 				echo $this->_sql();exit();
+//  				echo $this->_sql();
 // 				echo "<pre>";
 // 				print_r($data['order_list']);
 // 				echo "</pre>";
