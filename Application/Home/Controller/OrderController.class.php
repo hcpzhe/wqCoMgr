dd5c7e67eee1acf4839dd53b3d3578257679cfc1
wcd <774241885@qq.com>
2014-10-14 09:34:04 +0800

    带续费订单操作方式更改， 添加搜索

--- Application/Home/Controller/OrderController.class.php
+++ Application/Home/Controller/OrderController.class.php
@@ -36,8 +36,29 @@ class OrderController extends HomeBaseController{
 	}
 /** 带续费订单列表 (距订单到期时间三个月进入带续费期)*/
 	public function renewal_order(){
+		/*获取搜索条件*/
+		$pro=$_POST['product'];
+		$check=$_POST['check'];
+		$key=$_POST['key'];
+		/** 拼接where条件 */
+		if(!empty($pro)){
+			$where=$where." AND pt.id=".$pro;
+		}
+		/*通过，待审核*/
+		if($check==1){
+			$where=$where." AND oe.check=1";
+		}else if($check==2){
+			$where=$where." AND oe.check=0";
+		}else{ $where=$where." AND oe.check>=0";}
+		
+		if(!empty($key)){
+			$where=$where." AND ( pt.name like '%".$key."%' or cr.name like '%".$key."%' or ur.realname like '%".$key."%')";
+		}
 		$order=new OrderModel();
-		$this->rene=$order->renelist();
+		$this->rene=$order->renelist($where);
+		/*产品列表*/
+		$product=new ProductModel();
+		$this->pro_list=$product->p_list();
 		$this->display();
 	}	
 /**   订单详情   */
@@ -74,7 +95,7 @@ class OrderController extends HomeBaseController{
 		else{ $this->error('保存失败');}
 	}	
 /** 跳转到添加订单页面*/
-	public function add_order_form(){
+	public function add_order_form(){/** 判断是否传出客户id */
 		/*查询产品分类*/
 		$product=new ProductModel();
 		$this->p_list=$product->p_list();
