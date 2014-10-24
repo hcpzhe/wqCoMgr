022713065848ce93f17a532f79c49db3f781cd2a
zhaoyuhua <2072224807@qq.com>
2014-10-24 17:19:47 +0800

    待续费订单列表 搜索完善

--- Application/Home/Controller/OrderController.class.php
+++ Application/Home/Controller/OrderController.class.php
@@ -40,25 +40,24 @@ class OrderController extends HomeBaseController{
 /** 待续费订单列表 (距订单到期时间三个月进入带续费期)*/
 	public function renewal_order(){
 		/*获取搜索条件*/
-		$pro=$_POST['product'];
-		$check=$_POST['check'];
-		$key=$_POST['key'];
+		$pro = (int)I('param.product');
+		$check = (int)I('param.check');
+		$key = I('param.key');
 		/** 拼接where条件 */
 		if(!empty($pro)){
 			$where=$where." AND pt.id=".$pro;
 		}
 		/*通过，待审核*/
-		if($check==1){
+		if(!empty($check) && $check==1){
 			$where=$where." AND oe.check=1";
-		}else if($check==2){
+		}else if(!empty($check) && $check==2){
 			$where=$where." AND oe.check=0";
-		}else{ $where=$where." AND oe.check>=0";}
-		
+		}		
 		if(!empty($key)){
-			$where=$where." AND ( pt.name like '%".$key."%' or cr.name like '%".$key."%' or ur.realname like '%".$key."%')";
+			$where=$where." AND ( cr.name like '%".$key."%' or ur.realname like '%".$key."%')";
 		}
 		$order=new OrderModel();
-		$this->rene=$order->renelist($where);
+		$this->data=$order->renelist($where);
 		/*产品列表*/
 		$product=new ProductModel();
 		$this->pro_list=$product->p_list();
