6f0083073ba4463ac14fe831c142203ceb9152b9
wcd <774241885@qq.com>
2014-11-04 11:13:31 +0800

    修改订单付款记录中时间储存方式为时间戳，更改时间显示方式 增加 添加订单付款记录功能

--- Application/Home/Controller/OrderpayController.class.php
+++ Application/Home/Controller/OrderpayController.class.php
@@ -2,6 +2,7 @@
 namespace Home\Controller;
 use Common\Controller\HomeBaseController;
 use Home\Model\Order_payModel;
+use Home\Model\OrderModel;
 class OrderpayController extends HomeBaseController{
 	/*订单付款记录*/
 	public function order_pay_record(){
@@ -37,10 +38,28 @@ class OrderpayController extends HomeBaseController{
 	public function up(){
 		$id=$_GET['id'];
 		$data['money']=$_POST['money'];
-		$data['pay_time']=$_POST['pay_time'];
+		$data['pay_time']=strtotime($_POST['pay_time']);
 		$orderpay=new Order_payModel();
 		$flag=$orderpay->where("id=$id")->save($data);
 		if($flag==1){ $this->success('提交成功');}
 		else{ $this->error('提交成功');};
+	}
+	/** 添加订单付款记录 */
+	public function ad_form(){
+		$id=$_GET['id'];
+		$order=new OrderModel();
+		$this->orderlinfo=$order->orderinfo($id);
+		$this->display();
+	}
+	/** 添加记录  */
+	public function ad_record(){
+		$map["order_id"]=$_GET["id"];
+		$map["money"]=$_POST["money"];
+		$map["class"]=2;
+		$map["pay_time"]=strtotime($_POST["pay_time"]);
+		$orderpay=new Order_payModel();
+		$flay=$orderpay->add($map);
+		if($flay==0){$this->error("添加失败");	}
+		else{ $this->success("添加成功");}
 	}
 }
